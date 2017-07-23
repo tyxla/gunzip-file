87c2580016e9b885582d77affa7b0b2b500e648d
Alberto Miranda <codealchemist@gmail.com>
2017-07-23 09:44:27 -0300

    Passing errors to callback.

--- test.js
+++ test.js
@@ -4,37 +4,57 @@ var fs = require('fs');
 var gunzip = require('./');
 
 tape('gunzip a file', function(t) {
-	var gzPath = __filename + '.gz';
-	var filePath = __filename.replace(/\.js$/, '-example\.js');
-	var gz = fs.createWriteStream(gzPath);
+  var gzPath = __filename + '.gz';
+  var filePath = __filename.replace(/\.js$/, '-example\.js');
+  var gz = fs.createWriteStream(gzPath);
 
-	fs.createReadStream(__filename)
-		.pipe(zlib.createGzip())
-		.pipe(gz);
+  fs.createReadStream(__filename)
+    .pipe(zlib.createGzip())
+    .pipe(gz);
 
-	gz.on('close', function() {
-		gunzip(gzPath, filePath, function() {
+  gz.on('close', function() {
+    gunzip(gzPath, filePath, function() {
 
-			var expected = fs.createReadStream(__filename);
-			expected.on('open', function() {
+      var expected = fs.createReadStream(__filename);
+      expected.on('open', function() {
 
-				var actual = fs.createReadStream(filePath);
-				actual.on('open', function() {
+        var actual = fs.createReadStream(filePath);
+        actual.on('open', function() {
 
-					t.same( actual.toString(), expected.toString() );
+          t.same( actual.toString(), expected.toString() );
 
-					expected.close();
-					actual.close();
+          expected.close();
+          actual.close();
 
-					fs.unlink(gzPath);
-					fs.unlink(filePath);
+          fs.unlinkSync(gzPath);
+          fs.unlinkSync(filePath);
 
-					t.end();
-					
-				});
+          t.end();
 
-			});
-			
-		});
-	});
+        });
+
+      });
+
+    });
+  });
+});
+
+tape('return errors: source file does not exist', function(t) {
+  gunzip('invalid/path/file.gz', '/tmp/output-file.txt', function(err) {
+    t.same(err, 'Source file does not exist! invalid/path/file.gz');
+    t.end();
+  });
+});
+
+tape('return errors: source file is an invalid gzip', function(t) {
+  var sourceFile = __filename + '.gz';
+  var destFile = __filename + '.txt'
+  fs.writeFileSync(sourceFile, 'Invalid gzip content.');
+
+  gunzip(sourceFile, destFile, function(err) {
+    t.same(err.toString(), 'Error: incorrect header check');
+    fs.unlinkSync(sourceFile);
+    fs.unlinkSync(destFile);
+    t.end();
+  });
 });
